3cf6ee5828d4a37b9679ba1053c89d9a47eeb5fb
Nuhser <martinimmel@gmail.com>
2021-12-01 16:00:31 +0100

    Add first solution script

---
+++ 1/solution1.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    with open('./input1.txt') as input_file:
+        depths = []
+
+        for line in input_file.readlines():
+            depths.append(line.strip())
+
+        input_file.close()
+
+    depths = [int(d) for d in depths]
+
+    n_increases = 0
+    last_depth = depths[0]
+    for depth in depths[1 :]:
+        if last_depth < depth:
+            n_increases += 1
+        last_depth = depth
+
+    print(f'Total dpth increasses: {n_increases}')
