006a674847936ee2420a4caec98c0f3bf91199f9
Nuhser <martinimmel@gmail.com>
2022-12-05 15:52:29 +0100

    Improve util script

--- 2022/aoc_util.py
+++ 2022/aoc_util.py
@@ -1,26 +1,32 @@
-def parse_input(day: int, *delimiters: str, strip_lines: bool=True, cast_to: type=str, use_test: bool=False) -> list:
-    with open(f"./input{day:02d}.txt" if not use_test else f"./test{day:02d}.txt", "r") as puzzle_input:
-        if len(delimiters) == 0:
-            return [cast_to(line.strip() if strip_lines else line) for line in puzzle_input.readlines()]
-        else:
-            return [recursive_split(line.strip() if strip_lines else line, delimiters, cast_to) for line in puzzle_input.readlines()]
+def get_puzzle_input(day: int):
+    with open(f"./input{day:02d}.txt", "r") as puzzle_input:
+        return [line for line in puzzle_input.readlines()]
 
-def parse_input_with_blocks(day: int, *line_delimiters: str, block_delimiter: str="", strip_lines: bool=True, cast_to: type=str, use_test: bool=False) -> list[list]:
+def get_test_input(day: int):
+    with open(f"./test{day:02d}.txt", "r") as test_input:
+        return [line for line in test_input.readlines()]
+
+def parse_input(puzzle_input: list[str], *delimiters: str, strip_lines: bool=True, cast_to: type=str, use_test: bool=False) -> list:
+    if len(delimiters) == 0:
+        return [cast_to(line.strip() if strip_lines else line) for line in puzzle_input]
+    else:
+        return [recursive_split(line.strip() if strip_lines else line, delimiters, cast_to) for line in puzzle_input]
+
+def parse_input_with_blocks(puzzle_input: list[str], *line_delimiters: str, block_delimiter: str="", strip_lines: bool=True, cast_to: type=str, use_test: bool=False) -> list[list]:
     blocks = [[]]
-    with open(f"./input{day:02d}.txt" if not use_test else f"./test{day:02d}.txt", "r") as puzzle_input:
-        for line in [line.strip() if strip_lines else line for line in puzzle_input.readlines()]:
-            if line == block_delimiter:
-                blocks.append([])
-                continue
+    for line in [line.strip() if strip_lines else line for line in puzzle_input]:
+        if line == block_delimiter:
+            blocks.append([])
+            continue
 
-            if len(line_delimiters) == 0:
-                blocks[-1].append(cast_to(line))
-            else:
-                blocks[-1].append([recursive_split(line, line_delimiters, cast_to) for line in puzzle_input.readlines()])
+        if len(line_delimiters) == 0:
+            blocks[-1].append(cast_to(line))
+        else:
+            blocks[-1].append([recursive_split(line, line_delimiters, cast_to) for line in puzzle_input])
 
     return blocks
 
-def recursive_split(item: str, delimiters: tuple, cast_to: type):
+def recursive_split(item: str, delimiters: tuple, cast_to: type) -> list:
     if len(delimiters) <= 1:
         return [cast_to(subitem) for subitem in item.split(delimiters[0])]
     else:
