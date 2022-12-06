c063915a4692d8b410f7d96ffb2eedbd6096784e
Nuhser <martinimmel@gmail.com>
2022-12-06 14:42:03 +0100

    Make split on whitespace possible

--- aoc_util.py
+++ aoc_util.py
@@ -28,9 +28,9 @@ def parse_input_with_blocks(puzzle_input: list[str], *line_delimiters: str, bloc
 
 def recursive_split(item: str, delimiters: tuple, cast_to: type) -> list:
     if len(delimiters) <= 1:
-        return [cast_to(subitem) for subitem in item.split(delimiters[0])]
+        return [cast_to(subitem) for subitem in (item.split(delimiters[0]) if delimiters[0] != "" else item.split())]
     else:
-        return [recursive_split(subitem, delimiters[1:], cast_to) for subitem in item.split(delimiters[0])]
+        return [recursive_split(subitem, delimiters[1:], cast_to) for subitem in (item.split(delimiters[0]) if delimiters[0] != "" else item.split())]
 
 def split_string_in_chunks(string: str, chunk_size: int, padding_size: int=0) -> list[str]:
     chunks = []
