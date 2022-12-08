02842ab87677265c7151ec862a7d06d9afbb99ac
Nuhser <martinimmel@gmail.com>
2022-12-08 11:43:55 +0100

    Add console argument for tracking computation time

--- aoc.py
+++ aoc.py
@@ -1,8 +1,10 @@
 import aoc_util as aoc
 import argparse
 import importlib
+import time
 
 if __name__ == "__main__":
+    # create parser and parse console arguments
     parser = argparse.ArgumentParser(
         prog = "python3 run.py",
         description = "",
@@ -12,8 +14,8 @@ if __name__ == "__main__":
 
     parser.add_argument("year", type=int, metavar="YEAR", help="year to use")
     parser.add_argument("day", type=int, metavar="DAY", help="day to use")
+    parser.add_argument("--time", action="store_true", dest="track_time", help="track the time it takes to parse the input and compute the solutions/visualization")
     parser.add_argument("-v", "--verbose", action="store_true", dest="verbose", help="show more logs while running")
-    parser.add_argument("--version", action="version", version="v22.01.1", help="show the version of this program")
 
     run_parser = subparsers.add_parser("run")
     run_parser.add_argument("-t", "--test", action="store_true", dest="run_is_test", help="run is a test and should use the test input and solutions")
@@ -23,11 +25,18 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    parse_time = 0
+    part1_time = 0
+    part2_time = 0
+    visualization_time = 0
+
     # check if run is test
     run_is_test = (args.subcommand == "run") and args.run_is_test
 
     if (args.subcommand == "run"):
         print(f"{'Testing' if run_is_test else 'Executing'} year {args.year} day {args.day}...")
+    else:
+        print("Starting visualization...")
 
     # get puzzle/test input
     try:
@@ -50,57 +59,89 @@ if __name__ == "__main__":
             raise AttributeError("No expected test result found in your test file for part 2 of the solution! Make sure that the correct number of expected results is given at the start of the file (e.g.: #!part2:<RESULT>).")
 
     # create solution object for given day and year
+    if args.track_time:
+        parse_time = time.time()
+
     try:
         solution = importlib.import_module(f"{args.year}.day{args.day:02d}").Solution(args.year, args.day, puzzle_input, args.verbose)
     except ModuleNotFoundError:
         raise ModuleNotFoundError(f"There is no solution module for day {args.day} of year {args.year}! Create a module named '{args.year}/day{args.day:02d}.py'")
 
+    if args.track_time:
+        parse_time = time.time() - parse_time
+        print(f"\u001b[7mParsing took {parse_time:.5f} seconds to complete\u001b[0m")
+
     # start visualization and exit program if wanted
     if (args.subcommand == "visualize"):
-        print("Starting visualization...")
+        if args.track_time:
+            visualization_time = time.time()
+
         solution.visualize()
-        raise SystemExit
+        print("\nVisualization complete")
 
-    # run part 1
-    if (args.part == None) or (args.part == 1):
-        print("\nPart 1:")
+        if args.track_time:
+            visualization_time = time.time() - visualization_time
+            print(f"\u001b[7mVisualization took {visualization_time:.5f} seconds to complete\u001b[0m")
 
-        try:
-            solution_string, raw_solution = solution.part1()
-        except (NotImplementedError, RuntimeError) as error:
-            print(f"\u001b[31;1mERROR: {error}\u001b[0m")
-        else:
-            print("\u001b[34;1m" + solution_string + "\u001b[0m")
+    # else start computation run
+    else:
+        # run part 1
+        if (args.part == None) or (args.part == 1):
+            print("\nPart 1:")
 
-            # check if solutions equals the expected test result
-            if run_is_test:
-                assert expected_results != None
+            if args.track_time:
+                part1_time = time.time()
 
-                if expected_results["part1"] == None:
-                    print("\u001b[34;1mSolution not testable.\u001b[0m")
-                elif expected_results["part1"] == str(raw_solution):
-                    print("\u001b[32;1mThis solution is correct!\u001b[0m")
-                else:
-                    print("\u001b[31;1mThis solution is incorrect! Expected solution: \u001b[4m" + expected_results["part1"] + "\u001b[0m")
+            try:
+                solution_string, raw_solution = solution.part1()
+            except (NotImplementedError, RuntimeError) as error:
+                print(f"\u001b[31;1mERROR: {error}\u001b[0m")
+            else:
+                print("\u001b[34;1m" + solution_string + "\u001b[0m")
 
-    # run part 2
-    if (args.part == None) or (args.part == 2):
-        print("\nPart 2:")
+                # check if solutions equals the expected test result
+                if run_is_test:
+                    assert expected_results != None
 
-        try:
-            solution_string, raw_solution = solution.part2()
-        except (NotImplementedError, RuntimeError) as error:
-            print(f"\u001b[31;1mERROR: {error}\u001b[0m")
-        else:
-            print("\u001b[34;1m" + solution_string + "\u001b[0m")
+                    if expected_results["part1"] == None:
+                        print("\u001b[34;1mSolution not testable.\u001b[0m")
+                    elif expected_results["part1"] == str(raw_solution):
+                        print("\u001b[32;1mThis solution is correct!\u001b[0m")
+                    else:
+                        print("\u001b[31;1mThis solution is incorrect! Expected solution: \u001b[4m" + expected_results["part1"] + "\u001b[0m")
 
-            # check if solutions equals the expected test result
-            if run_is_test:
-                assert expected_results != None
+            if args.track_time:
+                part1_time = time.time() - part1_time
+                print(f"\u001b[7mPart 1 took {part1_time:.5f} seconds to complete\u001b[0m")
 
-                if expected_results["part2"] == None:
-                    print("\u001b[34;1mSolution not testable.\u001b[0m")
-                elif expected_results["part2"] == str(raw_solution):
-                    print("\u001b[32;1mThis solution is correct!\u001b[0m")
-                else:
-                    print("\u001b[31;1mThis solution is incorrect! Expected solution: \u001b[4m" + expected_results["part2"] + "\u001b[0m")
+        # run part 2
+        if (args.part == None) or (args.part == 2):
+            print("\nPart 2:")
+
+            if args.track_time:
+                part2_time = time.time()
+
+            try:
+                solution_string, raw_solution = solution.part2()
+            except (NotImplementedError, RuntimeError) as error:
+                print(f"\u001b[31;1mERROR: {error}\u001b[0m")
+            else:
+                print("\u001b[34;1m" + solution_string + "\u001b[0m")
+
+                # check if solutions equals the expected test result
+                if run_is_test:
+                    assert expected_results != None
+
+                    if expected_results["part2"] == None:
+                        print("\u001b[34;1mSolution not testable.\u001b[0m")
+                    elif expected_results["part2"] == str(raw_solution):
+                        print("\u001b[32;1mThis solution is correct!\u001b[0m")
+                    else:
+                        print("\u001b[31;1mThis solution is incorrect! Expected solution: \u001b[4m" + expected_results["part2"] + "\u001b[0m")
+
+            if args.track_time:
+                part2_time = time.time() - part2_time
+                print(f"\u001b[7mPart 2 took {part2_time:.5f} seconds to complete\u001b[0m")
+
+    if args.track_time:
+        print(f"\n\u001b[7mTotal compute time: {(parse_time + part1_time + part2_time + visualization_time):.5f} seconds\u001b[0m")
