09076b5017a08f8a7d6fca88f3ad47a972df5cfe
Martin Immel <immel@weclapp.com>
2022-12-13 13:41:10 +0100

    Add selection sort

--- sorting.py
+++ sorting.py
@@ -1,3 +1,5 @@
+# TODO: Selection, Quick, Merge, Shell, Heap, Stooge
+
 from typing import Any, Callable, TypeVar
 
 # used for generics
@@ -33,5 +35,19 @@ def insertion_sort(sorting_list: list[T], comparison_function: Callable[[T, T], 
             j -= 1
 
         sorted_list[j + 1] = key
+
+    return sorted_list
+
+def selection_sort(sorting_list: list[T], comparison_function: Callable[[T, T], (bool | None)]) -> list[T]:
+    # Avg.: O(n^2), Best: O(n^2), Worst: O(n^2)
+
+    sorted_list = sorting_list.copy()
+    for idx in range(len(sorted_list)):
+        min_idx = idx
+        for i in range(idx + 1, len(sorted_list)):
+            if not comparison_function(sorted_list[i], sorted_list[min_idx]):
+                min_idx = i
+
+        sorted_list[idx], sorted_list[min_idx] = sorted_list[min_idx], sorted_list[idx]
 
     return sorted_list
