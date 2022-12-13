887f1a1cedb3107e8a4b08f7fd2017d731afff4a
Martin Immel <immel@weclapp.com>
2022-12-13 13:30:47 +0100

    Add insertion sort

--- sorting.py
+++ sorting.py
@@ -4,6 +4,8 @@ from typing import Any, Callable, TypeVar
 T = TypeVar("T")
 
 def bubble_sort(sorting_list: list[T], comparison_function: Callable[[T, T], (bool | None)]) -> list[T]:
+    # Avg.: O(n^2), Best: O(n), Worst: O(n^2)
+
     sorted_list = sorting_list.copy()
 
     swapped = False
@@ -15,5 +17,21 @@ def bubble_sort(sorting_list: list[T], comparison_function: Callable[[T, T], (bo
         
         if not swapped:
             break
+
+    return sorted_list
+
+def insertion_sort(sorting_list: list[T], comparison_function: Callable[[T, T], (bool | None)]) -> list[T]:
+    # Avg.: O(n^2), Best: O(n), Worst: O(n^2)
+
+    sorted_list = sorting_list.copy()
+    for i in range(1, len(sorted_list)):
+        key = sorted_list[i]
+
+        j = i - 1
+        while (j >= 0) and (not comparison_function(sorted_list[j], key)):
+            sorted_list[j + 1] = sorted_list[j]
+            j -= 1
+
+        sorted_list[j + 1] = key
 
     return sorted_list
