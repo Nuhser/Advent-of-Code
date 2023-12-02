653ce173c35db637e65d6aa25281a45b5cc70aa4
Nuhser <martinimmel@gmail.com>
2023-12-02 13:43:58 +0100

    Day 1 - Add part 2

--- 2023/day01.py
+++ 2023/day01.py
@@ -7,18 +7,44 @@ from typing import override
 class Solution(aoc.AbstractSolution):
     @override
     def parse(self, puzzle_input: list[str]) -> None:
-        self.numbers: list[int] = []
-        for line in puzzle_input:
-            digits = [char for char in line if char.isdigit()]
-            self.numbers.append(int(digits[0] + digits[-1]))
-
-        if (self.verbose):
-            print(f"Numbers: {", ".join(str(number) for number in self.numbers)}")
+        self.puzzle_input = puzzle_input
         
     @override
     def part1(self) -> tuple[str, (int | float | str | None)]:
-        return f"The sum of all the calibration values is {sum(self.numbers)}", sum(self.numbers)
+        numbers: list[int] = []
+        for line in self.puzzle_input:
+            digits = [char for char in line if char.isdigit()]
+            numbers.append(int(digits[0] + digits[-1]))
+
+        if (self.verbose):
+            print(f"Numbers: {", ".join(str(number) for number in numbers)}")
+
+        return f"The sum of all the calibration values is {sum(numbers)}", sum(numbers)
 
     @override
     def part2(self) -> tuple[str, (int | float | str | None)]:
-        raise NotImplementedError(f"Part 2 of the solution for day {self.day} of year {self.year} isn't implemented yet!")
+        # left characters before and after every number in case of numbers sharing a letter in the input file
+        digits_dict = {
+            "one": "o1e",
+            "two": "t2o",
+            "three": "t3e",
+            "four": "f4r",
+            "five": "f5e",
+            "six": "s6x",
+            "seven": "s7n",
+            "eight": "e8t",
+            "nine": "n9e"
+        }
+
+        numbers = []
+        for line in self.puzzle_input:
+            for key in digits_dict:
+                line = line.replace(key, str(digits_dict[key]))
+
+            digits = [char for char in line if char.isdigit()]
+            numbers.append(int(digits[0] + digits[-1]))
+
+        if (self.verbose):
+            print(f"Numbers: {", ".join(str(number) for number in numbers)}")
+
+        return f"The sum of all the calibration values is {sum(numbers)}", sum(numbers)
