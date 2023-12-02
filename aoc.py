febd02db757ae06cbdc606f4dc86e67378230387
Nuhser <martinimmel@gmail.com>
2023-12-02 14:30:52 +0100

    Improve formatting

--- aoc.py
+++ aoc.py
@@ -119,10 +119,12 @@ class Solution(aoc.AbstractSolution):
     @override
     def parse(self, puzzle_input: list[str]) -> None:
         pass
-        
+
+
     @override
     def part1(self) -> tuple[str, (int | float | str | None)]:
         raise NotImplementedError(f"Part 1 of the solution for day {self.day} of year {self.year} isn't implemented yet!")
+
 
     @override
     def part2(self) -> tuple[str, (int | float | str | None)]:
