193daf333aeab0ed68266a5053fce056e598f269
Nuhser <martinimmel@gmail.com>
2023-12-02 12:26:47 +0100

    Add 2023 day 1

--- 2023/README.md
+++ 2023/README.md
@@ -6,4 +6,4 @@ Each day consists of one input file containing my personal randomly generated pu
 
 ## This Year's Tasks
 
-1. [](https://github.com/Nuhser/Advent-of-Code/blob/master/2023/day01.py) (*[original task](https://adventofcode.com/2023/day/1)*)
+1. [Trebuchet?!](https://github.com/Nuhser/Advent-of-Code/blob/master/2023/day01.py) (*[original task](https://adventofcode.com/2023/day/1)*)
---
+++ 2023/day01.py
@@ -0,0 +1,17 @@
+import aoc_util as aoc
+
+from typing import override
+
+
+class Solution(aoc.AbstractSolution):
+    @override
+    def parse(self, puzzle_input: list[str]) -> None:
+        pass
+        
+    @override
+    def part1(self) -> tuple[str, (int | float | str | None)]:
+        raise NotImplementedError(f"Part 1 of the solution for day {self.day} of year {self.year} isn't implemented yet!")
+
+    @override
+    def part2(self) -> tuple[str, (int | float | str | None)]:
+        raise NotImplementedError(f"Part 2 of the solution for day {self.day} of year {self.year} isn't implemented yet!")
