e7553fc9fd052f994d080255eb903f51d1b7e8d6
Martin Immel <immel@weclapp.com>
2023-12-04 09:50:14 +0100

    Day 4 - Add part 1

--- 2023/day04.py
+++ 2023/day04.py
@@ -1,17 +1,38 @@
-import aoc_util as aoc
-
+from collections import defaultdict
 from typing import override
-
+import aoc_util as aoc
 
 class Solution(aoc.AbstractSolution):
     @override
     def parse(self, puzzle_input: list[str]) -> None:
-        pass
+        self.cards: list[tuple[defaultdict[int, bool], list[int]]] = []
+
+        parsed_input = aoc.parse_input(puzzle_input, ": ", " | ", "")
+        for card in parsed_input:
+            winning_numbers: list[int] = [int(n.strip()) for n in card[1][0]]
+            numbers: list[int] = [int(n.strip()) for n in card[1][1]]
+
+            self.cards.append((defaultdict(lambda: False), numbers))
+
+            for number in winning_numbers:
+                self.cards[-1][0][number] = True
 
 
     @override
     def part1(self) -> tuple[str, (int | float | str | None)]:
-        raise NotImplementedError(f"Part 1 of the solution for day {self.day} of year {self.year} isn't implemented yet!")
+        total_points: int = 0
+
+        for card in self.cards:
+            numbers_found: int = 0
+
+            for number in card[1]:
+                if (card[0][number]):
+                    numbers_found += 1
+
+            if (numbers_found > 0):
+                total_points += 2 ** (numbers_found - 1)
+
+        return f"Total points: {total_points}", total_points
 
 
     @override
