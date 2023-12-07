5ac8ab620542dd7f110067ba069aaa398b0609be
Martin Immel <immel@weclapp.com>
2023-12-07 11:19:27 +0100

    Day 6 - Add part 2

--- 2023/day06.py
+++ 2023/day06.py
@@ -6,23 +6,21 @@ import aoc_util as aoc
 class Solution(aoc.AbstractSolution):
     @override
     def parse(self, puzzle_input: list[str]) -> None:
-        lines = aoc.parse_input(puzzle_input, "")
+        lines: list[str] = aoc.parse_input(puzzle_input, "")
 
         self.races: list[dict[str, int]] = []
         for idx in range(1, len(lines[0])):
             self.races.append({"time": int(lines[0][idx]), "record": int(lines[1][idx])})
+
+        self.time_part_2: int = int(puzzle_input[0].strip().replace(" ", "").split(":")[1])
+        self.record_part_2: int = int(puzzle_input[1].strip().replace(" ", "").split(":")[1])
 
 
     @override
     def part1(self) -> tuple[str, (int | float | str | None)]:
         number_of_wins: list[int] = []
         for race in self.races:
-            x = sqrt(((-race["time"] / 2) ** 2) - (race["record"] + 1))
-
-            x1 = floor((race["time"] / 2) + x)
-            x2 = ceil((race["time"] / 2) - x)
-
-            number_of_wins.append(abs(int(x1 - x2)) + 1)
+            number_of_wins.append(self.get_number_of_possible_wins(race["time"], race["record"]))
 
         margin = prod(number_of_wins)
 
@@ -31,4 +29,15 @@ class Solution(aoc.AbstractSolution):
 
     @override
     def part2(self) -> tuple[str, (int | float | str | None)]:
-        raise NotImplementedError(f"Part 2 of the solution isn't implemented yet!")
+        number_of_wins: int = self.get_number_of_possible_wins(self.time_part_2, self.record_part_2)
+
+        return f"Number of wins: {number_of_wins}", number_of_wins
+
+
+    def get_number_of_possible_wins(self, time: int, record: int) -> int:
+        x = sqrt(((-time / 2) ** 2) - (record + 1))
+
+        x1 = floor((time / 2) + x)
+        x2 = ceil((time / 2) - x)
+
+        return abs(int(x1 - x2)) + 1
