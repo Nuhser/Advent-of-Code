3d5c6bf620fda31d62b7fbbe9a787461f8d4cecf
Martin Immel <immel@weclapp.com>
2023-12-08 11:54:13 +0100

    Day 8 - Add part 2

--- 2023/day08.py
+++ 2023/day08.py
@@ -1,3 +1,4 @@
+from math import lcm
 from typing import override
 import aoc_util as aoc
 
@@ -14,15 +15,15 @@ class Solution(aoc.AbstractSolution):
             self.nodes[line[0][0]] = {"L": line[1][0], "R": line[1][1][:-1]}
 
         if self.verbose:
-            print(f"Instructions: {self.instructions}")
+            print(f"Instructions:\n{self.instructions}")
             print(f"Nodes:\n{self.nodes}")
 
 
     @override
     def part1(self) -> tuple[str, (int | float | str | None)]:
-        current_node = "AAA"
-        step = 0
-        found_destination = False
+        current_node: str = "AAA"
+        step: int = 0
+        found_destination: bool = False
 
         while not found_destination:
             for instruction in self.instructions:
@@ -38,4 +39,28 @@ class Solution(aoc.AbstractSolution):
 
     @override
     def part2(self) -> tuple[str, (int | float | str | None)]:
-        raise NotImplementedError(f"Part 2 of the solution isn't implemented yet!")
+        start_nodes: list[str] = [node for node in self.nodes.keys() if node[-1] == "A"]
+        steps: list[int] = []
+
+        for start_node in start_nodes:
+            current_node: str = start_node
+            step: int = 0
+            found_destination: bool = False
+
+            while not found_destination:
+                for instruction in self.instructions:
+                    if (current_node[-1] == "Z"):
+                        found_destination = True
+                        break
+
+                    step += 1
+                    current_node = self.nodes[current_node][instruction]
+
+            steps.append(step)
+
+        if self.verbose:
+            print(f"Steps to Destinations: {steps}")
+
+        final_step = lcm(*steps)
+
+        return f"Found all destinations at the same time after {final_step} steps.", final_step
