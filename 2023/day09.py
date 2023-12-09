3d821ab071f786ad1caf8ddc69fcbb664333b6f4
Nuhser <martinimmel@gmail.com>
2023-12-09 10:42:14 +0100

    Day 9 - Make faster
    Compute the differences in the histories only once during the parsing.

--- 2023/day09.py
+++ 2023/day09.py
@@ -5,13 +5,10 @@ import aoc_util as aoc
 class Solution(aoc.AbstractSolution):
     @override
     def parse(self, puzzle_input: list[str]) -> None:
-        self.histories = aoc.parse_input(puzzle_input, " ", cast_to=int)
+        histories = aoc.parse_input(puzzle_input, " ", cast_to=int)
+        self.history_diffs: list[list[list[int]]] = []
 
-
-    @override
-    def part1(self) -> tuple[str, (int | float | str | None)]:
-        predictions: list[int] = []
-        for history in self.histories:
+        for history in histories:
             differences: list[list[int]] = [history.copy()]
             current_values: list[int] = history.copy()
 
@@ -22,6 +19,13 @@ class Solution(aoc.AbstractSolution):
 
                 current_values = differences[-1].copy()
 
+            self.history_diffs.append(differences)
+
+
+    @override
+    def part1(self) -> tuple[str, (int | float | str | None)]:
+        predictions: list[int] = []
+        for differences in self.history_diffs:
             prediction: int = 0
             for difference in differences[-2 :: -1]:
                 prediction += difference[-1]
@@ -39,17 +43,7 @@ class Solution(aoc.AbstractSolution):
     @override
     def part2(self) -> tuple[str, (int | float | str | None)]:
         predictions: list[int] = []
-        for history in self.histories:
-            differences: list[list[int]] = [history.copy()]
-            current_values: list[int] = history.copy()
-
-            while any(value != 0 for value in current_values):
-                differences.append([])
-                for i in range(len(current_values) - 1):
-                    differences[-1].append(current_values[i+1] - current_values[i])
-
-                current_values = differences[-1].copy()
-
+        for differences in self.history_diffs:
             prediction: int = 0
             for difference in differences[-2 :: -1]:
                 prediction = difference[0] - prediction
