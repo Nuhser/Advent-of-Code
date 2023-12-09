93210142307cbe5431e4a3f54f4cfcf55fdb224e
Nuhser <martinimmel@gmail.com>
2023-12-09 10:34:27 +0100

    Day 9 - Add part 2

--- 2023/day09.py
+++ 2023/day09.py
@@ -38,4 +38,27 @@ class Solution(aoc.AbstractSolution):
 
     @override
     def part2(self) -> tuple[str, (int | float | str | None)]:
-        return super().part2()
+        predictions: list[int] = []
+        for history in self.histories:
+            differences: list[list[int]] = [history.copy()]
+            current_values: list[int] = history.copy()
+
+            while any(value != 0 for value in current_values):
+                differences.append([])
+                for i in range(len(current_values) - 1):
+                    differences[-1].append(current_values[i+1] - current_values[i])
+
+                current_values = differences[-1].copy()
+
+            prediction: int = 0
+            for difference in differences[-2 :: -1]:
+                prediction = difference[0] - prediction
+
+            predictions.append(prediction)
+
+        if self.verbose:
+            print(f"Predictions for first numbers: {predictions}")
+
+        predictions_sum: int = sum(predictions)
+
+        return f"Sum of predictions: {predictions_sum}", predictions_sum
