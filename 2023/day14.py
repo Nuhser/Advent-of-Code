749afcf93e3a4b5509037032b3dcdf33f9cb8673
Nuhser <martinimmel@gmail.com>
2023-12-14 22:55:08 +0100

    Day 14 - Add part 1

--- 2023/day14.py
+++ 2023/day14.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from typing import override
-from utility.mapping import generate_map_with_coordinates
+from utility.mapping import generate_map_with_coordinates, get_map_dimensions, get_row_of_map, print_map
 import aoc_util as aoc
 
 
@@ -12,7 +12,32 @@ class Solution(aoc.AbstractSolution):
 
     @override
     def part1(self) -> tuple[str, (int | float | str | None)]:
-        most_north_rocks: list[int] = [-1 if (rock == self.RockType.NONE) else 0 for rock in self.platform]
+        most_north_rocks: list[int] = [-1 if (rock == self.RockType.NONE) else 0 for _, rock in get_row_of_map(self.platform, 0)]
+        _, y_len = get_map_dimensions(self.platform)
+
+        for y in range(1, y_len):
+            for (x, _), rock in get_row_of_map(self.platform, y):
+                match rock:
+                    case self.RockType.NONE:
+                        continue
+                    case self.RockType.SQUARE:
+                        most_north_rocks[x] = y
+                    case self.RockType.ROUND:
+                        self.platform[x, y] = self.RockType.NONE
+                        self.platform[x, most_north_rocks[x] + 1] = self.RockType.ROUND
+                        most_north_rocks[x] += 1
+
+        if self.verbose:
+            print("Platform after tilting north:")
+            print_map(self.platform, end="\n")
+
+        weight: int = 0
+        for y in range(y_len):
+            for _, rock in get_row_of_map(self.platform, y):
+                if rock == self.RockType.ROUND:
+                    weight += y_len - y
+
+        return f"Total load on north support beams: {weight}", weight
 
 
     @override
@@ -24,3 +49,6 @@ class Solution(aoc.AbstractSolution):
         NONE = "."
         ROUND = "O"
         SQUARE = "#"
+
+        def __str__(self):
+            return self.value
