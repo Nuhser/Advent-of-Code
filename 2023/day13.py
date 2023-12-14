4be8284966b0e23d38f2f2f4951a9cc7f3545773
Martin Immel <immel@weclapp.com>
2023-12-14 16:07:11 +0100

    Day 13 - Start part 1

--- 2023/day13.py
+++ 2023/day13.py
@@ -5,12 +5,20 @@ import aoc_util as aoc
 class Solution(aoc.AbstractSolution):
     @override
     def parse(self, puzzle_input: list[str]) -> None:
-        pass
+        self.patterns = aoc.parse_input_with_blocks(puzzle_input)
 
 
     @override
     def part1(self) -> tuple[str, (int | float | str | None)]:
-        return super().part1()
+        for pattern in self.patterns:
+            # check for horizontal mirror
+            for idx in range(0, len(pattern) - 1):
+                top_part_size = idx + 1
+
+                if (idx + top_part_size > len(pattern)):
+                    bottom_part_size = top_part_size - (idx + top_part_size - len(pattern))
+
+                if (pattern[ : top_part_size] == pattern[top_part_size : min(2*top_part_size, len(pattern)) : -1])
 
 
     @override
