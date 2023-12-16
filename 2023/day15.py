620de17463457d8578d5879016e83e234b701bdb
Nuhser <martinimmel@gmail.com>
2023-12-16 09:40:59 +0100

    Day 15 - Add part 2

--- 2023/day15.py
+++ 2023/day15.py
@@ -5,12 +5,24 @@ import aoc_util as aoc
 class Solution(aoc.AbstractSolution):
     @override
     def parse(self, puzzle_input: list[str]) -> None:
-        pass
+        self.sequence = aoc.parse_input(puzzle_input, ",")[0]
 
 
     @override
     def part1(self) -> tuple[str, (int | float | str | None)]:
-        return super().part1()
+        total_sum:int = 0
+
+        for code in self.sequence:
+            test_sum:int = 0
+
+            for char in code:
+                test_sum += ord(char)
+                test_sum *= 17
+                test_sum %= 256
+
+            total_sum += test_sum
+
+        return f"The test sum is {total_sum}", total_sum
 
 
     @override
