475d572c0bb2ef4e673284b2d2e760aaff7c2cba
Martin Immel <immel@weclapp.com>
2024-12-03 10:36:38 +0100

    Day 2 - Add solution for part 2

--- 2024/day02.py
+++ 2024/day02.py
@@ -1,5 +1,6 @@
 from typing import override
 import aoc_util as aoc
+from utility.terminal_formatting import Color
 
 
 class Solution(aoc.AbstractSolution):
@@ -13,30 +14,7 @@ class Solution(aoc.AbstractSolution):
         safe_reports: int = 0
 
         for report in self.reports:
-            is_safe: bool = True
-            last_level: int = report[0]
-            is_increase: bool = None
-
-            for level in report[1:]:
-                diff: int = level - last_level
-
-                if abs(diff) > 3:
-                    is_safe = False
-                    break
-
-                if (diff == 0):
-                    is_safe = False
-                    break
-
-                if (is_increase == None):
-                    is_increase = (diff > 0)
-                elif (is_increase != (diff > 0)):
-                    is_safe = False
-                    break
-
-                last_level = level
-
-            if (is_safe):
+            if (self.check_report_safeness(report)):
                 safe_reports += 1
 
         return f"Number of safe reports: {safe_reports}", safe_reports
@@ -44,4 +22,67 @@ class Solution(aoc.AbstractSolution):
 
     @override
     def part2(self) -> tuple[str, (int | float | str | None)]:
-        return super().part2()
+        safe_reports: int = 0
+
+        for idx, report in enumerate(self.reports):
+            if (self.verbose):
+                print(f"{Color.YELLOW}Analyzing report {idx}...{Color.DEFAULT}")
+
+            is_safe: bool = self.check_report_safeness(report)
+
+            if (is_safe):
+                safe_reports += 1
+
+            else:
+                if (self.verbose):
+                    print(f"{Color.YELLOW}Removing problematic levels...{Color.DEFAULT}")
+
+                for i in range(len(report)):
+                    new_report: list[int] = report[: i] + report[i+1 :]
+
+                    if (self.check_report_safeness(new_report)):
+                        safe_reports += 1
+                        break
+
+            if (self.verbose):
+                print("")
+
+        return f"Number of safe reports: {safe_reports}", safe_reports
+
+
+    def check_level_safeness(self, level: int, last_level: int, is_increase: bool | None) -> tuple[bool, int, bool | None]:
+        diff: int = level - last_level
+        last_level = level
+
+        if (diff == 0):
+            return False, last_level, is_increase
+
+        if (is_increase == None):
+            is_increase = (diff > 0)
+        elif (is_increase != (diff > 0)):
+            return False, last_level, is_increase
+
+        if abs(diff) > 3:
+            return False, last_level, is_increase
+
+        return True, last_level, is_increase
+    
+
+    def check_report_safeness(self, report: list[int]) -> bool:
+        is_safe: bool = True
+        last_level: int = report[0]
+        is_increase: bool | None = None
+
+        for idx, level in enumerate(report[1:]):
+            is_safe, last_level, is_increase = self.check_level_safeness(level, last_level, is_increase)
+
+            if (not is_safe):
+                if (self.verbose):
+                    print(f"{Color.RED}Level {idx} or {idx+1} is not safe.{Color.DEFAULT}")
+
+                return False
+
+        if (self.verbose):
+            print(f"{Color.GREEN}All levels are safe.{Color.DEFAULT}")
+
+        return True
