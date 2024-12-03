29e7b194a43d88e8cab602d9aaeebed96d843f68
Martin Immel <immel@weclapp.com>
2024-12-03 11:22:28 +0100

    Day 3 - Add solution for part 1

--- 2024/day03.py
+++ 2024/day03.py
@@ -1,16 +1,24 @@
 from typing import override
 import aoc_util as aoc
+import re
 
 
 class Solution(aoc.AbstractSolution):
     @override
     def parse(self, puzzle_input: list[str]) -> None:
-        pass
+        self.puzzle_input = puzzle_input
 
 
     @override
     def part1(self) -> tuple[str, (int | float | str | None)]:
-        return super().part1()
+        correct_instructions: list[str] = [match for line in self.puzzle_input for match in re.findall(r"mul\(\d{1,3},\d{1,3}\)", line)]
+
+        sum: int = 0
+        for instruction in correct_instructions:
+            numbers = [int(n) for n in instruction.lstrip("mul(").rstrip(")").split(",")]
+            sum += numbers[0] * numbers[1]
+
+        return f"Sum: {sum}", sum
 
 
     @override
