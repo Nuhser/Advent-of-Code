c43f8a57bdb30bae8a240ddd68f7fbfa6655f844
Martin Immel <immel@weclapp.com>
2024-12-04 16:26:17 +0100

    Day 4 - Refactor part 1

--- 2024/day04.py
+++ 2024/day04.py
@@ -1,5 +1,6 @@
 from typing import override
 import aoc_util as aoc
+import utility.mapping as mapping
 
 
 class Solution(aoc.AbstractSolution):
@@ -10,13 +11,11 @@ class Solution(aoc.AbstractSolution):
 
     @override
     def part1(self) -> tuple[str, (int | float | str | None)]:
-        words_found: int = 0
+        xmas_found: int = 0
 
         for coords, letter in self.puzzle_input.items():
             if (letter != "X"):
                 continue
-
-            # print(f"Found X at {coords}")
 
             letter_m_list = mapping.get_matching_neighbors(
                 self.puzzle_input,
@@ -24,22 +23,16 @@ class Solution(aoc.AbstractSolution):
                 lambda _, n: n[1] == "M"
             )
 
-            # print(f"Found following Ms: {letter_m_list}")
-
             for (x, y), _ in letter_m_list:
                 (dx, dy) = (x - coords[0], y - coords[1])
 
-                # print(f"Direction: {(dx, dy)}")
-
                 try:
                     if (self.puzzle_input[(x + dx), (y + dy)] == "A") and (self.puzzle_input[(x + 2 * dx), (y + 2 * dy)] == "S"):
-                        words_found += 1
+                        xmas_found += 1
                 except KeyError:
                     pass
 
-            # break
-
-        return f"Words found: {words_found}", words_found
+        return f"Words found: {xmas_found}", xmas_found
 
 
     @override
