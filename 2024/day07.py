3154765602ed313e48ca6bc5c9551a4d4a248c2c
Martin Immel <immel@weclapp.com>
2024-12-09 14:57:43 +0100

    Day 7 - Add solution for part 2

--- 2024/day07.py
+++ 2024/day07.py
@@ -5,11 +5,16 @@ import aoc_util as aoc
 class Solution(aoc.AbstractSolution):
     @override
     def parse(self, puzzle_input: list[str]) -> None:
-        self.equations: list[tuple[int, list[int]]] = [(input[0][0], input[1]) for input in aoc.parse_input(puzzle_input, ": ", " ", cast_to=int)]
+        self.equations: list[tuple[int, list[int]]] = [
+            (input[0][0], input[1])
+            for input in aoc.parse_input(puzzle_input, ": ", " ", cast_to=int)
+        ]
 
     @override
     def part1(self) -> tuple[str, (int | float | str | None)]:
-        possible_solutions: list[list[int]] = [self.get_possible_solutions(equation[1]) for equation in self.equations]
+        possible_solutions: list[list[int]] = [
+            self.get_possible_solutions(equation[1]) for equation in self.equations
+        ]
 
         correct_solutions_sum: int = 0
         for idx, solutions in enumerate(possible_solutions):
@@ -18,20 +23,57 @@ class Solution(aoc.AbstractSolution):
                     correct_solutions_sum += solution
                     break
 
-        return f"Total Calibration Result: {correct_solutions_sum}", correct_solutions_sum
+        return (
+            f"Total Calibration Result: {correct_solutions_sum}",
+            correct_solutions_sum,
+        )
 
     @override
     def part2(self) -> tuple[str, (int | float | str | None)]:
-        return super().part2()
+        possible_solutions: list[list[int]] = [
+            self.get_possible_solutions(equation[1], True)
+            for equation in self.equations
+        ]
 
-    def get_possible_solutions(self, inputs: list[int]) -> list[int]:
+        correct_solutions_sum: int = 0
+        for idx, solutions in enumerate(possible_solutions):
+            for solution in solutions:
+                if self.equations[idx][0] == solution:
+                    correct_solutions_sum += solution
+                    break
+
+        return (
+            f"Total Calibration Result: {correct_solutions_sum}",
+            correct_solutions_sum,
+        )
+
+    def get_possible_solutions(
+        self, inputs: list[int], with_concatenation: bool = False
+    ) -> list[int]:
         if len(inputs) == 1:
             return inputs
 
-        return [
-            inputs[-1] + possible_solution
-            for possible_solution in self.get_possible_solutions(inputs[:-1])
-        ] + [
-            inputs[-1] * possible_solution
-            for possible_solution in self.get_possible_solutions(inputs[:-1])
-        ]
+        return (
+            [
+                inputs[-1] + possible_solution
+                for possible_solution in self.get_possible_solutions(
+                    inputs[:-1], with_concatenation
+                )
+            ]
+            + [
+                inputs[-1] * possible_solution
+                for possible_solution in self.get_possible_solutions(
+                    inputs[:-1], with_concatenation
+                )
+            ]
+            + (
+                [
+                    int(str(possible_solution) + str(inputs[-1]))
+                    for possible_solution in self.get_possible_solutions(
+                        inputs[:-1], with_concatenation
+                    )
+                ]
+                if with_concatenation
+                else []
+            )
+        )
