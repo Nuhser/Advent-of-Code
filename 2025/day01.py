741a834e0cc9e6a7fc589da7ceda355ba06422d3
Martin Immel <martin.immel@exact.com>
2025-12-01 09:01:48 +0100

    Day 1 - Solve part 1

--- 2025/day01.py
+++ 2025/day01.py
@@ -5,12 +5,23 @@ import aoc_util as aoc
 class Solution(aoc.AbstractSolution):
     @override
     def parse(self, puzzle_input: list[str]) -> None:
-        pass
+        commands: list[str] = aoc.parse_input(puzzle_input)
+        self.commands = [int(command.replace("R", "").replace("L", "-")) for command in commands]
 
 
     @override
     def part1(self) -> tuple[str, (int | float | str | None)]:
-        return super().part1()
+        count: int = 0
+        position: int = 50
+
+        for command in self.commands:
+            position += command
+            position %= 100
+
+            if (position == 0):
+                count += 1
+
+        return f"The arrow is pointing {count} times at 0.", count
 
 
     @override
