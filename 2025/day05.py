6db53bab2f2f51cfa5f4118b66c1c76441897c48
Martin Immel <martin.immel@exact.com>
2025-12-05 07:15:43 +0100

    Day 5 - Add solution for part 1

--- 2025/day05.py
+++ 2025/day05.py
@@ -5,12 +5,21 @@ import aoc_util as aoc
 class Solution(aoc.AbstractSolution):
     @override
     def parse(self, puzzle_input: list[str]) -> None:
-        pass
+        database = aoc.parse_input_with_blocks_and_block_specific_line_delimiters(puzzle_input, ('-',), (), cast_to=int)
+        self.ingredient_ranges: list[list[int]] = database[0]
+        self.ingredient_ids: list[int] = database[1]
 
 
     @override
     def part1(self) -> tuple[str, (int | float | str | None)]:
-        return super().part1()
+        fresh_ingredients: int = 0
+        for id in self.ingredient_ids:
+            for range in self.ingredient_ranges:
+                if (range[0] <= id) and (range[1] >= id):
+                    fresh_ingredients += 1
+                    break
+
+        return f"There are {fresh_ingredients} fresh ingredients available.", fresh_ingredients
 
 
     @override
